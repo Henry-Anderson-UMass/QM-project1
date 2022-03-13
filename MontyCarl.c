c2d3d25726e248ceb03def9f0b887e4f4678e193
Michael Ash <mash@econs.umass.edu>
2022-03-13 11:50:14 -0400

    Streamlined search of array, also added more reporting

--- MontyCarl.c
+++ MontyCarl.c
@@ -7,6 +7,10 @@
 #include "OceanCell.h"
 
 
+// Searching the correct OceanCell has a chance of success.
+#define success_prob 0.46
+
+
 // Create a function to return a Uniform[0,1] random variable
 double get_random() { 
     return ((double)rand() / (double)RAND_MAX); 
@@ -35,17 +39,15 @@ int setRandomLocation() {
 // chance of success.
 int searchCell(struct OceanCell input) {
   int found = 0 ;
-  if (input.subHere == 1) {  
-    // TEST: always finds sub if there
-    // return 1;
-    float success_prob = 0.46 ;
-    found = get_random() < success_prob ;
+  float dieroll = get_random() ;
+  if (input.subHere == 1) {
+    printf("You're in the right place but don't know it yet.\nNeed below %4.3f, got %4.3f\n", success_prob, dieroll) ;
+    found = (dieroll < success_prob) ;
   }
   return found ;
 }
 
 // Searches the array for the location of the sub. 
-// Searching the correct OceanCell has a 46% chance of success.
 // Parameters: the array to be searched and returnArr, an int array of size >=2.
 // Updates the values stored in returnArr according to the results of the search:
 // returnArr[0] is the location of the sub or -1 if not found.
@@ -53,77 +55,35 @@ int searchCell(struct OceanCell input) {
 // This function employs on the helper function searchCell(), which performs the
 // search and determines if the sub was found there according to the 46% probability of success.
 
-void searchArray(struct OceanCell searchArea[], int  returnArr[]) {
+void searchArray(struct OceanCell searchArea[], int returnArr[]) {
     int found = 0;
     int numSearches = 0;
-    int searchLeft;
-    int searchRight;
-
 
     // Should be able to avoid the rightside/leftside stuff with
-    int curCell = 200 ; // initialize before loop
-    for (int i = 0; i <= 200; i++) {
-      curCell = curCell + pow(-1,i)*(i+1) ; // make this the last line in the loop 200+1-2+3-4+5-6,... etc. 
-      // Set the cells to be searched according to i, the search offset.
-      searchLeft = 200 - i;
-      searchRight = 200 + i;
-      int found = 0;
-        
-        // Edge case for searching the last cell.
-        // If the search offset, i, ==200,
-        // we've already searched the entire right side of the array.
-        // So, we only need to search the left.
-        if (i == 200) {
-            
-            if (searchArea[searchLeft].searched == 0) {
-                searchArea[searchLeft].searched = 1;
-                found = searchCell(searchArea[searchLeft]);
-                numSearches++;
-            }
-
-            if (found == 1) {
-                returnArr[0] = searchLeft;
-                returnArr[1] = numSearches;
-                return;
-            } else if (found == 0) {
-                break;
-            }
-
-        }
-
-        // Searches the left side of the array and handles searching the same cell multiple times.
-        if (searchArea[searchLeft].searched == 0) {
-            searchArea[searchLeft].searched = 1;
-            found = searchCell(searchArea[searchLeft]);
-            numSearches++;
-        }
-
-        if (found == 1) {
-            returnArr[0] = searchLeft;
-            returnArr[1] = numSearches;
-            return;
-        }
-
-        // Searches the right side of the array and handles searching the same cell multiple times.
-        if (searchArea[searchRight].searched == 0) {
-            searchArea[searchRight].searched = 1;
-            found = searchCell(searchArea[searchRight]);
-            numSearches++;
-        }
-
-        if (found == 1) {
-            returnArr[0] = searchRight;
-            returnArr[1] = numSearches;
-            return;
-        }        
+    int curCell = 200 ; // initialize 
+    for (int i = 0; i < 400; i++) {
+      if (i<5 || i>395) {
+	printf("Iteration %i Searching cell %i\n", i, curCell) ;
+      }
+      // Seach current cell
+      searchArea[curCell].searched++ ;
+      found = searchCell(searchArea[curCell]);
+      numSearches++;
+      
+      if (found == 1) {
+	returnArr[0] = curCell;
+	returnArr[1] = numSearches;
+	break ;
+      } else {
+	//Return if the submarine was not found.
+	returnArr[0] = -1;
+	returnArr[1] = numSearches;
+      }
+      // Update current cell
+      curCell = curCell + pow(-1,i)*(i+1) ; // make this the last line in the loop 200+1-2+3-4+5-6,... etc.
     }
-
-    //Returns if the submarine was not found.
-    returnArr[0] = -1;
-    returnArr[1] = numSearches;
-    return;
-
 }
+
 
 void main() {
     // Input a new seed to the rand() function every time the program
@@ -136,7 +96,6 @@ void main() {
     int searchReturn [2];
 
     struct OceanCell searchArea [400];
-
 
     // Initialize all values to zero.
     for (int i = 0; i < 400; i++) {
@@ -152,18 +111,19 @@ void main() {
         searchArray(searchArea, searchReturn);
         searchCounter = searchCounter + searchReturn[1];
 
-        // Reset "searched" params in the array.
-        for (int i = 0; i < 400; i++) {
-            searchArea[i].searched = 0;
-        }
+        /* // Reset "searched" params in the array. */
+        /* for (int i = 0; i < 400; i++) { */
+        /*     searchArea[i].searched = 0; */
+        /* } */
     } while(searchReturn[0] == -1);
 
     // TESTING: 
     printf("You conducted %i cell searches. \n", searchCounter);
+    // Next line is not working.
+    printf("You searched cell %i %i times. \n", searchReturn[0], searchArea[searchReturn[0]].searched );
     printf("You found the sub in cell %i, where we expected it in cell %i. \n", searchReturn[0], location);
 
     // TODO: Main should run the setup and search many thousands of times and write searchCounter
     // out to a text file suitable for import to R for statistical analysis.
     
 }
-
